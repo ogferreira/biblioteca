a49909861c49464b16014650c89fa1384fe18847
Gabriel Ferreira <gabrielferresilveira@gmail.com>
2023-07-28 16:16:28 -0300

    versão com rotas

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send('Legal');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('Bem-vindo ao IFC');
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Eu amo praia');
+});
+
 module.exports = router;
